a982eb7970cc35676080f8b35c32cc6cec9bbb9c
Andy Ruda <andygabru9@gmail.com>
2022-02-17 21:16:55 -0500

    final

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,61 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Renders App", () => {
+  it("must render without crashing", () => {
+    render(<App />);
+    screen.getByText(/TCS Tecnical test/i);
+  });
+
+  it("must show Output while inputs are correct", () => {
+    render(<App />);
+    const wordInput = screen.getByLabelText(/Please insert a word to find/i);
+    const textInput = screen.getByLabelText(/Please insert a Text/i);
+    fireEvent.change(wordInput, { target: { value: "word" } });
+    fireEvent.change(textInput, {
+      target: { value: "word word noword word word no word" },
+    });
+    screen.getByText(/was found 5 times in 7 words/i);
+  });
+
+  it("must parce comas, points & white spaces", () => {
+    render(<App />);
+    const wordInput = screen.getByLabelText(/Please insert a word to find/i);
+    fireEvent.change(wordInput, { target: { value: ", . , any word" } });
+    screen.getByText(/anyword/i);
+  });
+
+  it("must count mayus & minus as same", () => {
+    render(<App />);
+    const wordInput = screen.getByLabelText(/Please insert a word to find/i);
+    const textInput = screen.getByLabelText(/Please insert a Text/i);
+    fireEvent.change(wordInput, { target: { value: "WoRd" } });
+    fireEvent.change(textInput, {
+      target: { value: "WORD word noWord W,oRd word no wor" },
+    });
+    screen.getByText(/was found 3 times in 8 words/i);
+  });
+
+  it("must not show Output while all inputs are incorrect", () => {
+    render(<App />);
+    const wordInput = screen.getByLabelText(/Please insert a word to find/i);
+    const textInput = screen.getByLabelText(/Please insert a Text/i);
+    fireEvent.change(wordInput, { target: { value: "" } });
+    fireEvent.change(textInput, { target: { value: "" } });
+    screen.getByText(/Please Check Word and Text inputs/i);
+  });
+
+  it("must not show Output while word input are incorrect", () => {
+    render(<App />);
+    const wordInput = screen.getByLabelText(/Please insert a word to find/i);
+    fireEvent.change(wordInput, { target: { value: "" } });
+    screen.getByText(/Please Check Word and Text inputs/i);
+  });
+
+  it("must not show Output while text input are incorrect", () => {
+    render(<App />);
+    const textInput = screen.getByLabelText(/Please insert a Text/i);
+    fireEvent.change(textInput, { target: { value: "" } });
+    screen.getByText(/Please Check Word and Text inputs/i);
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Container, Box, TextField, Typography } from "@mui/material";
+
+const lorem =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci eu lobortis elementum nibh tellus molestie Lorem. Commodo elit at imperdiet dui accumsan sit amet nulla. Nunc sed velit dignissim sodales ut eu. Lacus vel facilisis volutpat est velit egestas dui. In hendrerit gravida rutrum quisque non tellus orci ac auctor. Egestas sed sed risus pretium Lorem vulputate. Nibh ipsum consequat nisl vel pretium lectus quam. Blandit volutpat maecenas volutpat blandit aliquam etiam. Quisque sagittis purus sit amet Lorem consequat mauris nunc. Commodo ullamcorper a lacus Lorem sed. Elit eget gravida cum Lorem natoque penatibus et. Nibh mauris cursus Lorem mattis leo in vitae turpis massa sed elementum.";
 
 function App() {
+  const [word, setWord] = useState("lorem");
+  const [text, setText] = useState(lorem);
+
+  const [totalWords, setTotalWords] = useState(0);
+  const [wordMatchs, setWordMatchs] = useState(0);
+
+  useEffect(() => {
+    if (word.length > 1 && text.length > 1) {
+      //Lowercase word to find
+      const wordLowerCase = word.toLowerCase();
+      //Remove all comas & points
+      const cleanText = text.replace(/[.,\n]/g, " ");
+      //Make Array, Remove white spaces & lowercase
+      const allWordsInText = cleanText
+        .split(" ")
+        .filter((w) => w !== "")
+        .map((w) => w.toLowerCase());
+      //Count all matchs
+      const wordMatchs = allWordsInText.filter(
+        (w) => w === wordLowerCase
+      ).length;
+      setTotalWords(allWordsInText.length);
+      setWordMatchs(wordMatchs);
+    }
+  }, [word, text]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component="main" maxWidth="sm">
+      {/* Spacing to top */}
+      <Box sx={{ paddingTop: "20%" }} />
+      {/* Center */}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h2" sx={{ marginBottom: "20px" }}>
+          TCS Tecnical test
+        </Typography>
+
+        <TextField
+          sx={{ alignSelf: "flex-end" }}
+          margin="normal"
+          required
+          id="word"
+          label="Please insert a word to find"
+          name="word"
+          value={word}
+          onChange={(e) => setWord(e.target.value.replace(/[,.\n\s]/g, ""))}
+          autoFocus
+        />
+
+        <TextField
+          margin="normal"
+          required
+          multiline
+          rows={5}
+          fullWidth
+          id="text"
+          label="Please insert a Text"
+          name="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        {/* Output */}
+        {word.length > 1 && text.length > 1 ? (
+          <Typography component="h5" variant="h5" sx={{ marginTop: "5%" }}>
+            🌐 <b>"{word}"</b>{` was found ${wordMatchs} times in ${totalWords} words`}
+          </Typography>
+        ) : (
+          <Typography component="h5" variant="h5" sx={{ marginTop: "5%" }}>
+            Please Check Word and Text inputs ⚠️
+          </Typography>
+        )}
+      </Box>
+    </Container>
   );
 }
 
